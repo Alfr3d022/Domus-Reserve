19c2fd53249e1a3426b4ba69ad6853fd8e946e22
alf <fredimpina@gmail.com>
2023-11-22 19:35:56 -0300

    conexão com o node

--- dbServer.js
+++ dbServer.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
 const mysql = require("mysql")
+
 const db = mysql.createPool({
    connectionLimit: 100,
-   host: "127.0.0.1",
-   user: "root",
-   password: "",
+   host: "26.153.51.36",
+   user: "alfredogay",
+   password: "123",
    database: "dbDomus",
    port: "3306"
 })
@@ -18,11 +19,17 @@ app.listen(port, ()=> console.log(`Server rodando na porta:  ${port}...`))
 
 app.use(express.json())
 
-app.post("/criarUsuario", async (req,res) => {
+app.use(express.static('src'))
+
+app.get("/",function (req,res){
+   res.sendFile(__dirname + '/src/index.html')
+})
+
+app.post("/add-registro", async (req,res) => {
 const zero = 0
-const user = req.body.name;
-const hashedPassword = req.body.password;
-const email =  req.body.email;
+const user = req.body.login;
+const hashedPassword = req.body.senha;
+const email =  req.body.confsenha;
 db.getConnection( async (err, connection) => {
  if (err) throw (err)
  const sqlSearch = "SELECT * FROM tab_users WHERE user_name = ?"
---
+++ src/js/popup.js
@@ -0,0 +1,10 @@
+function abrirModal(){
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click',(e) => {
+        if(e.target.id == 'fechar'|| e.target.id == 'janela-modal'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
