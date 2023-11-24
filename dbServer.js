07322957ab116d49eabc2f79f6c0f5d4d7e4029a
JaimeeCJ <jaimecaetano312@gmail.com>
2023-11-23 21:30:40 -0300

    This commit removes a duplicate implementation of the login route.

--- dbServer.js
+++ dbServer.js
@@ -86,41 +86,6 @@ app.post("/login", async (req, res) => {
 })
 
 
-app.post("/login", async (req, res) => {
-   const email = req.body.email;
-   const senha = req.body.senha;
-
-   db.getConnection(async (err, connection) => {
-      if (err) throw (err)
-      const sqlSearch = "SELECT * FROM tab_users WHERE user_email = ?"
-      const search_query = mysql.format(sqlSearch, [email])
-
-      await connection.query(search_query, async (err, result) => {
-         if (err) throw (err)
-         console.log("------> Procurando usuários")
-         console.log(result.length)
-
-         if (result.length === 0) {
-            connection.release();
-            console.log("------> Usuário não existe");
-            return res.sendStatus(401);
-         }
-
-         const storedHashedPassword = result[0].user_pass;
-
-         if (senha != storedHashedPassword) {
-            connection.release();
-            console.log("------> Senha incorreta");
-            return res.sendStatus(401);
-         }
-
-         connection.release();
-         console.log("------> Login bem-sucedido");
-         res.sendStatus(200);
-      })
-   })
-})
-
 app.post("/retornaSelect", async (req, res) => {
    db.getConnection(async (err, connection) => {
       if (err) throw (err);
